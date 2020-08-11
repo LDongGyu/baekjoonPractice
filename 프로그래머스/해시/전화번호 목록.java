8497988a521da5a010825733ec13a7adbeac25af
LDongGyu <ehdrbchd@naver.com>
2020-08-11 21:40:09 +0900

    프로그래머스 해시:전화번호 목록

---
+++ 프로그래머스/해시/전화번호 목록.java
@@ -0,0 +1,19 @@
+package programmers;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        for(int i = 0; i < phone_book.length; i++){
+            for(int j = 0; j < phone_book.length; j++){
+                if(i != j){
+                    if(phone_book[i].startsWith(phone_book[j])){
+                        return false;   
+                    }
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
