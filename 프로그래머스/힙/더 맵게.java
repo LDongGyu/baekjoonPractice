d0ba01742c6137e8d96fb02110787ffc8dde42ac
LDongGyu <ehdrbchd@naver.com>
2020-08-25 22:34:04 +0900

    프로그래머스 힙:더 맵게

---
+++ 프로그래머스/힙/더 맵게.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> scovilleQueue = new PriorityQueue<>();
+        
+        for(int i = 0; i < scoville.length; i++){
+            scovilleQueue.offer(scoville[i]);
+        }
+        
+        while(scovilleQueue.peek() < K){
+            if(scovilleQueue.size() == 1){
+                return -1;
+            }
+            int first = scovilleQueue.poll();
+            int second = scovilleQueue.poll();
+            
+            scovilleQueue.offer(first+second*2);
+            answer++;
+        }
+        
+        return answer;
+    }
+}
