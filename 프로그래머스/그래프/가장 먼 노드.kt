5712b07f0f140657d89bccca857ff2a6e9150788
LDongGyu <ehdrbchd@naver.com>
2020-09-14 22:10:50 +0900

    프로그래머스 그래프:가장 먼 노드 Kotlin BFS 통과

--- 프로그래머스/그래프/가장 먼 노드.kt
+++ 프로그래머스/그래프/가장 먼 노드.kt
@@ -1,52 +1,44 @@
-class Solution {
-    lateinit var map:Array<IntArray>
-    lateinit var visit:BooleanArray
-    var nodeN = 0
-    var min = Integer.MAX_VALUE
+import java.util.*
 
+class Solution {
     fun solution(n: Int, edge: Array<IntArray>): Int {
         var answer = 0
-        map = Array<IntArray>(n,{i->IntArray(n,{i->0})})
+        var edgeList = Array<MutableList<Int>>(n,{i-> mutableListOf()})
 
-        for(line in edge){
-            map[line[0]-1][line[1]-1] = 1
-            map[line[1]-1][line[0]-1] = 1
-        }
-        nodeN = n
-        visit = BooleanArray(n,{i->false})
-        visit[0] = true
-
-        var list = mutableListOf<Int>()
-
-        for(i in 1..n-1) {
-            dfs(0, i, 0)
-            list.add(min)
-            min = Integer.MAX_VALUE
+        for(e in edge){
+            edgeList[e[0]-1].add(e[1]-1)
+            edgeList[e[1]-1].add(e[0]-1)
         }
 
-        list.forEach {
-            if(it == list.max()) answer++
+        var cnt = IntArray(n)
+        var visited = BooleanArray(n)
+        var queue:Queue<Int> = LinkedList()
+
+        queue.add(0)
+        visited[0] = true
+
+        var now = 0
+        while(!queue.isEmpty()){
+            now = queue.poll()
+            for(node in edgeList[now]){
+                if(!visited[node]){
+                    cnt[node] = cnt[now] + 1
+                    visited[node] = true
+                    queue.add(node)
+                }
+            }
+        }
+
+        var max = 0
+        for (c in cnt){
+            if(max < c){
+                max = c
+                answer = 1
+            }
+            else if(max == c) answer++
         }
 
         return answer
-    }
-
-    fun dfs(start: Int, end: Int, cnt: Int){
-        if(start == end){
-            if(min > cnt){
-                min = cnt
-            }
-            return
-        }
-        if(cnt > nodeN) return
-
-        for(i in 1..nodeN-1){
-            if(!visit[i] && map[start][i] == 1){
-                visit[i] = true
-                dfs(i,end,cnt+1)
-                visit[i] = false
-            }
-        }
     }
 }
 
@@ -60,3 +52,55 @@ fun main() {
 
     print(result)
 }
+
+//class Solution {
+//    lateinit var map:Array<IntArray>
+//    lateinit var visit:BooleanArray
+//    var nodeN = 0
+//    var min = Integer.MAX_VALUE
+//
+//    fun solution(n: Int, edge: Array<IntArray>): Int {
+//        var answer = 0
+//        map = Array<IntArray>(n,{i->IntArray(n,{i->0})})
+//
+//        for(line in edge){
+//            map[line[0]-1][line[1]-1] = 1
+//            map[line[1]-1][line[0]-1] = 1
+//        }
+//        nodeN = n
+//        visit = BooleanArray(n,{i->false})
+//        visit[0] = true
+//
+//        var list = mutableListOf<Int>()
+//
+//        for(i in 1..n-1) {
+//            dfs(0, i, 0)
+//            list.add(min)
+//            min = Integer.MAX_VALUE
+//        }
+//
+//        list.forEach {
+//            if(it == list.max()) answer++
+//        }
+//
+//        return answer
+//    }
+//
+//    fun dfs(start: Int, end: Int, cnt: Int){
+//        if(start == end){
+//            if(min > cnt){
+//                min = cnt
+//            }
+//            return
+//        }
+//        if(cnt > nodeN) return
+//
+//        for(i in 1..nodeN-1){
+//            if(!visit[i] && map[start][i] == 1){
+//                visit[i] = true
+//                dfs(i,end,cnt+1)
+//                visit[i] = false
+//            }
+//        }
+//    }
+//}
