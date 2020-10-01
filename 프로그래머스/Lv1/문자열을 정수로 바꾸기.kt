ce7219d7a8bc80cc742e7729f44cf268ac302f24
LDongGyu <ehdrbchd@naver.com>
2020-10-01 21:39:44 +0900

    프로그래머스 Lv1:문자열을 정수로 바꾸기 Kotlin

---
+++ 프로그래머스/Lv1/문자열을 정수로 바꾸기.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun solution(s: String): Int {
+        return s.toInt()
+    }
+}
+//         val length = s.filter { it.isDigit() }.length
+fun main() {
+    var sol = Solution()
+
+    print(sol.solution("11111"))
+
+}
