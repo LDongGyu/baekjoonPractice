f8ecac26e566b17519db0334192ca4a1bd22899d
LDongGyu <ehdrbchd@naver.com>
2020-10-02 15:03:29 +0900

    프로그래머스 Lv1:자릿수 더하기 Kotlin

---
+++ 프로그래머스/Lv1/자릿수 더하기.kt
@@ -0,0 +1,20 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer = 0
+        var num = n
+
+        while(num > 0){
+            answer += num%10
+            num /= 10
+        }
+
+        return answer
+    }
+}
+
+fun main() {
+    var sol = Solution()
+
+    print(sol.solution(123))
+
+}
