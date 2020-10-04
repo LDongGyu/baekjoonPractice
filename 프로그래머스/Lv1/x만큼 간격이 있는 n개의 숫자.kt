a70cfa9dd0d771919e325c97d42a33fbbb8882bf
LDongGyu <ehdrbchd@naver.com>
2020-10-04 12:43:20 +0900

    프로그래머스 Lv1:x만큼 간격이 있는 n개의 숫자 Kotlin

---
+++ 프로그래머스/Lv1/x만큼 간격이 있는 n개의 숫자.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(x: Int, n: Int): LongArray {
+        var answer = LongArray(n)
+        for(i in 0..n-1){
+            answer[i] = x*(i+1).toLong()
+        }
+        return answer
+    }
+//    fun solution(x: Int, n: Int): LongArray = LongArray(n) { x.toLong() * (it + 1) }
+}
+
+fun main() {
+    var sol = Solution()
+
+    print(sol.solution(2,5))
+}
