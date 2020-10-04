dcd68474b326e535d536dde8362deeafae9cb3da
LDongGyu <ehdrbchd@naver.com>
2020-10-04 12:26:39 +0900

    프로그래머스 Lv1:직사각형 별찍기 Kotlin

---
+++ 프로그래머스/Lv1/직사각형 별찍기.kt
@@ -0,0 +1,20 @@
+//class Solution {
+//    fun solution(arr: IntArray): Double = arr.average()
+//}
+//
+//fun main() {
+//    var sol = Solution()
+//
+//    print(sol.solution(3))
+//}
+
+fun main(args: Array<String>) {
+    val (a, b) = readLine()!!.split(' ').map(String::toInt)
+
+    for(i in 1..b){
+        for(j in 1..a){
+            print('*')
+        }
+        print('\n')
+    }
+}
